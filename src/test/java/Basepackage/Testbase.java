b8dd7800570ed00e354f414824f4683b529c7f4f
Talha Chowdhury <tasimchy30@gmail.com>
2022-02-16 12:08:17 -0500

    prject

---
+++ src/test/java/Basepackage/Testbase.java
@@ -0,0 +1,21 @@
+package Basepackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class Testbase {
+	public WebDriver driver;
+	@Test
+	public void test() {
+		System.setProperty("webdriver.chrome.driver", "C:\\WebDriver97\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.homedepot.com/");
+		driver.manage().window().maximize();
+		driver.navigate().refresh();
+		driver.quit();
+	}
+	
+
+}
