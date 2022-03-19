b7c106a7fffe5a785c5e504a0e22a4b32a3f4b7e
Talha Chowdhury <tasimchy30@gmail.com>
2022-03-19 12:18:06 -0400

    cooment

--- src/test/java/Basepackage/Testbase.java
+++ src/test/java/Basepackage/Testbase.java
@@ -17,7 +17,7 @@ public class Testbase {
 		driver.get("https://www.homedepot.com/");
 		driver.manage().window().maximize();
 		driver.navigate().refresh();
-		
+		//comment
 	}
 	@AfterMethod
 	public void close() {
